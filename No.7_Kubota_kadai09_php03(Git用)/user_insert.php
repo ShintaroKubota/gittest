9c382c77a82f5f0d6d463f9a57d124e917a2cf26
Shintaro Kubota <shin25153@gmail.com>
2021-06-27 22:59:01 +0900

    No.7_Kubota_kadai09_php03(Git用)

---
+++ No.7_Kubota_kadai09_php03(Git用)/user_insert.php
@@ -0,0 +1,39 @@
+<?php
+// 1. POSTデータ取得
+//$name = filter_input( INPUT_GET, ","name" ); //こういうのもあるよ
+//$email = filter_input( INPUT_POST, "email" ); //こういうのもあるよ
+$name = $_POST["name"];
+$password = $_POST["password"];
+
+// 2. DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=bookmark;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+
+// ３．SQL文を用意(データ登録：INSERT)
+$stmt = $pdo->prepare(
+  "INSERT INTO userlists( id,name,password)
+  VALUES( NULL, :name, :password)"
+);
+
+// 4. バインド変数を用意
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':password', $password, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+
+// 5. 実行
+$status = $stmt->execute();
+
+// 6．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("ErrorMassage:".$error[2]);
+}else{
+  //５．bookmark.phpへリダイレクト
+  header('Location: user_select.php');
+}
+?>
